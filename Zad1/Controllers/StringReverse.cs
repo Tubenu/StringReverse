60a9acab3ad5c21adb77e51375faa9f76ea27ca2
Tubenu <rebel10002@gmail.com>
2019-12-02 17:10:46 +0100

    Add project files.

---
+++ Zad1/Controllers/StringReverse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Zad1.Controllers
+{
+    public class StringReverseController : ApiController
+    {
+        [Route("api/StringReverse/{paramOne}")]
+        public string Get(string paramOne)
+        {
+            char[] charArray = paramOne.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+
+        }
+    }
+}
